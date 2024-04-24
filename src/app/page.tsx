6c698ff00e187de635def4e541c43849a7e1d719
Mehmet Emin <mteminn@gmail.com>
2024-04-24 17:40:45 +0300

    cleared components and moved all to related pages instead, removed temporary useState hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,16 @@
 "use client"
 
-import { useState } from 'react';
+import PokeSidebar from '@/app/_components/PokeSidebar';
+import PokemonStats from '@/app/_components/PokemonStats';
+import usePokemonQuery from "@/app/_hooks/usePokemonQuery";
+import PokemonFrontBack from "@/app/_components/PokemonFrontBack";
 
-import Navbar from '../../components/navbar';
-import QueryStats from '../../components/query-stats';
 
 export default function Home() {
 
-  let [keyword, setKeyword] = useState('Ford Europe');
-  let [totalQueryResults, setTotalQueryResults] = useState('31,765');
+    return (
+        <div id="app-container">
 
-
-  return (
-    <div id="app-container" className="w-screen  grid grid-cols-12 mx-auto">
-
-      <Navbar></Navbar>
-
-      <main className='my-3 col-span-8 rounded bg-slate-900'>
-
-      </main>
-
-      <QueryStats keyword={keyword} totalQueryResults={totalQueryResults}></QueryStats>
-
-    </div >
-  )
+        </div>
+    )
 }
