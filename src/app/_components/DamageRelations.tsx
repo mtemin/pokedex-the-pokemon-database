079449cf2ef4a18d5181aa35c00d310a2580b48c
Mehmet Emin <mteminn@gmail.com>
2024-05-01 13:54:16 +0300

    added w-full and text-center classes

--- src/app/_components/DamageRelations.tsx
+++ src/app/_components/DamageRelations.tsx
@@ -4,7 +4,7 @@ import Skeleton from "@/app/_components/Skeleton";
 function DamageRelations({name}: { name: string }) {
     return (
         <p style={{backgroundColor: `var(--type-${name})`}}
-           className="text-[var(--background)] capitalize inline px-4 rounded mb-1">
+           className="text-[var(--background)] w-full capitalize inline text-center rounded mb-1">
             {name}
         </p>
     )
