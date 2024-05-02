f8165311fcaa18bc9a19fdc3cbdc049d5878fc84
Mehmet Emin <mteminn@gmail.com>
2024-05-02 14:00:32 +0300

    added tailwind-scrollbar plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/_components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/_components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        fontFamily: {},
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+    plugins: [
+        require('flowbite/plugin'),
+        require('tailwind-scrollbar'),
+    ],
 }
 export default config
