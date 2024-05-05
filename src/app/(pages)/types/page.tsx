d2ab3f25461a033e445b0357936d62c50c27b7cd
Mehmet Emin <mteminn@gmail.com>
2024-05-05 03:08:08 +0300

    pokemons by type page is now responsive

--- src/app/(pages)/types/page.tsx
+++ src/app/(pages)/types/page.tsx
@@ -2,63 +2,106 @@
 import React from 'react';
 import Image from "next/image";
 import Navbar from "@/app/_components/Navbar";
-
-const pokemonTypes = [
-    "Bug",
-    "Dark",
-    "Dragon",
-    "Electric",
-    "Fairy",
-    "Fighting",
-    "Fire",
-    "Flying",
-    "Ghost",
-    "Grass",
-    "Ground",
-    "Ice",
-    "Normal",
-    "Poison",
-    "Psychic",
-    "Rock",
-    "Steel",
-    "Water"
-]
+import IconSearch from "@/app/_components/Icon-Search";
+import useTypeQuery from "@/app/_hooks/useTypeQuery";
+import {useAppDispatch, useAppSelector} from "@/lib/hooks";
+import {setType} from "@/lib/features/selectedTypeSlice";
+import Link from "next/link";
 
 function Page() {
+    const dispatch = useAppDispatch()
+
+    const pokemonTypes = [
+        "Bug",
+        "Dark",
+        "Dragon",
+        "Electric",
+        "Fairy",
+        "Fighting",
+        "Fire",
+        "Flying",
+        "Ghost",
+        "Grass",
+        "Ground",
+        "Ice",
+        "Normal",
+        "Poison",
+        "Psychic",
+        "Rock",
+        "Steel",
+        "Water"
+    ]
+    // let selectedType = "fire";
+    let selectedType = useAppSelector((state: any) => state.selectedType.value)
+    const {
+        data: pokedexByTypeData,
+        isLoading: isPokedexByTypeLoading,
+        isError: isPokedexByTypeError
+    } = useTypeQuery(selectedType);
+    if (isPokedexByTypeLoading) {
+        console.log(`loading ${selectedType} pokemons`)
+    }
+    if (pokedexByTypeData) {
+
+        console.log(pokedexByTypeData.pokemon)
+    }
+
+    function getPokemonIdFromURL(url: string) {
+        let result = url.split("/");
+        return result[result.length - 2];
+    }
+
+    getPokemonIdFromURL("https://pokeapi.co/api/v2/pokemon/7/")
+
     return (
-        <section className="container mx-auto">
+        <main className="container mx-auto max-[640px]:p-4">
 
             <Navbar/>
-            <div id="filtering" className="container my-3 flex justify-between">
-                <p className="mr-auto font-medium text-lg">
-                    Filter By Types
-                </p>
-                <div className="types flex items-center gap-2">
-                    {pokemonTypes.map((type: string) =>
-                        <div key={type}
-                             className="flex flex-col justify-center items-center rounded transition border border-[var(--pokemon-bg-shadow)] duration-300 p-2"
-                        >
-                            <Image
-                                // onMouseEnter={() => {
-                                //     document.getElementById(`${type}-type-text`)?.classList.remove('hidden', 't')
-                                // }}
-                                // onMouseLeave={() => {
-                                //     document.getElementById(`${type}-type-text`)?.classList.add('hidden', 't')
-                                // }}
-
-                                id={`${type}-type-icon`} key={type} src={`/Pokemon_Type_Icon_${type}.png`}
-                                alt={`${type} Type Icon`}
-                                width={35}
-                                height={35} className="cursor-pointer z-[2] relative mb-auto"/>
-                            <p id={`${type}-type-text`}
-                               className="z-[1] relative mr-2 transition duration-300 text-center">
-                                {type}
-                            </p>
-                        </div>
-                    )}
-                </div>
+            <div className="flex justify-start items-center">
+                <input type="search" placeholder="Search for a pokemon..."
+                       className="relative max-sm:w-full text-[var(--foreground)] mt-4 mr-[-2rem] bg-[var(--background)] border-0 border-b-2 border-[var(--foreground-card)]"/>
+                <IconSearch className="w-8 h-8 absolute text-black bg-black z-[2]"/>
             </div>
-        </section>
+            <p className="mr-auto font-medium text-lg my-2">
+                Filter By Types
+            </p>
+            <section id="type-filters"
+                     className="types grid grid-cols-9 max-[800px]:grid-cols-6 max-[640px]:grid-cols-3 gap-2 my-2">
+                {pokemonTypes.map((type: string) =>
+                    <div key={type}
+                         onClick={() => {
+                             dispatch(setType({type}));
+                         }}
+                         className="flex flex-col justify-center items-center rounded transition cursor-pointer border border-[var(--pokemon-bg-shadow)] duration-300 p-2 hover:border-[var(--pokemon-fg)]"
+                    >
+                        <Image
+                            id={`${type}-type-icon`} key={type} src={`/Pokemon_Type_Icon_${type}.png`}
+                            alt={`${type} Type Icon`}
+                            width={35}
+                            height={35} className="cursor-pointer z-[2] relative mb-auto"/>
+                        <p id={`${type}-type-text`}
+                           className="z-[1] relative mr-2 transition duration-300 text-center">
+                            {type}
+                        </p>
+                    </div>
+                )}
+            </section>
+
+            <section id="pokedex-by-type">
+                {pokedexByTypeData &&
+                    pokedexByTypeData.pokemon.map((pokemon: any) =>
+                        <Link key={pokemon.id} href={`/pokemon/${getPokemonIdFromURL(pokemon.pokemon.url)}`}>
+                            <article style={{backgroundColor: `var(--type-${selectedType})`}}
+                                     className="flex flex-col justify-center items-center my-2 p-2 bg-[var(--pokemon-bg)] hover:shadow-inside transition-all duration-300">
+
+                                <p className="capitalize text-[var(--background)] font-medium">{pokemon.pokemon.name}</p>
+                            </article>
+                        </Link>
+                    )
+                }
+            </section>
+        </main>
+
 
     );
 }
