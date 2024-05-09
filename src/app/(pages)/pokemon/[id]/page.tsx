9ebc4f8c9d03b82651c71b3652a731c218691cdc
Mehmet Emin <mteminn@gmail.com>
2024-05-09 23:28:05 +0300

    moved PokemonShowcase wrapper element to inside component, also made this page responsive

--- src/app/(pages)/pokemon/[id]/page.tsx
+++ src/app/(pages)/pokemon/[id]/page.tsx
@@ -4,23 +4,27 @@ import PokemonDamageModifiers from "@/app/_components/PokemonDamageModifiers";
 import PokemonShowcase from "@/app/_components/PokemonShowcase";
 import PokemonStats from "@/app/_components/PokemonStats";
 import Navbar from "@/app/_components/Navbar";
+import usePokemonQuery from "@/app/_hooks/usePokemonQuery";
 
 function Pokemon({params}: any) {
+    let pokemonId = params.id;
+    const {isError: isPokemonError} = usePokemonQuery(pokemonId);
+    let mehmet = true;
 
     return (
-        <section className="h-screen overflow-hidden">
+        <section className="h-screen overflow-hidden max-[1000px]:overflow-y-scroll">
+            {/*{mehmet &&*/}
+            {/*    <div*/}
+            {/*        className="absolute bg-rose-500 w-[50%] h-[25%] z-[3] translate-x-[-50%] translate-y-[-50%] left-[50%] top-[50%] rounded-2xl flex justify-center items-center">eq</div>*/}
+            {/*}*/}
             <div className="mx-3">
                 <Navbar/>
             </div>
-            <div className="w-screen flex justify-between mx-auto">
+            <div className="w-screen flex flex-wrap  justify-between mx-auto">
                 <PokemonDamageModifiers pokemonId={params.id}></PokemonDamageModifiers>
-                <main className='text-center my-3 flex flex-grow mx-auto rounded bg-[var(--background-card)]'>
-                    <PokemonShowcase pokemonId={params.id}></PokemonShowcase>
-                </main>
-
+                <PokemonShowcase pokemonId={params.id}></PokemonShowcase>
                 <PokemonStats pokemonId={params.id}></PokemonStats>
             </div>
-
         </section>
     );
 }
