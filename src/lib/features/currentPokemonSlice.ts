0b3b9a6caa45e8f6f39d2cdb5be204ea1219d0f7
Mehmet Emin <mteminn@gmail.com>
2024-05-10 02:29:52 +0300

    new state for using in both navbar input and pokedex page

---
+++ src/lib/features/currentPokemonSlice.ts
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit'
+import type {PayloadAction} from '@reduxjs/toolkit'
+
+export type CurrentPokemonState = {
+    value: string | number
+}
+
+const initialState: CurrentPokemonState = {
+    value: "",
+}
+
+export const currentPokemonSlice = createSlice({
+    name: 'currentPokemon',
+    initialState,
+    reducers: {
+        setCurrentPokemon: (state, action) => {
+            state.value = (action.payload).toLowerCase();
+            console.log(action.payload)
+        },
+    },
+})
+
+export const {setCurrentPokemon} = currentPokemonSlice.actions
+
+export default currentPokemonSlice.reducer
