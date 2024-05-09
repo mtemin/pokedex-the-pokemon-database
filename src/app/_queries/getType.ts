94c7a504fbf00a7b9c6c6b50c3798dd68a3d8269
Mehmet Emin <mteminn@gmail.com>
2024-05-09 23:27:00 +0300

    added if block to prevent fetching when type is null

--- src/app/_queries/getType.ts
+++ src/app/_queries/getType.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export async function getType(type: string) {
-    return axios({
-        url: `https://pokeapi.co/api/v2/type/${type}`,
-        method: 'get',
-        headers: {"content-type": "application/json"},
-        // data: {"query": getTypeQuery}
-    })
+    if (type)
+        return axios({
+            url: `https://pokeapi.co/api/v2/type/${type}`,
+            method: 'get',
+            headers: {"content-type": "application/json"},
+            // data: {"query": getTypeQuery}
+        })
 };
-
