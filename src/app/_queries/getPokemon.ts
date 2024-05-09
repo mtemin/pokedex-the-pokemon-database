f8d1eb9751cc72f08fa0f966903c7c3696968561
Mehmet Emin <mteminn@gmail.com>
2024-05-09 23:05:24 +0300

    removed graphql query because endpoint doesnt support it

--- src/app/_queries/getPokemon.ts
+++ src/app/_queries/getPokemon.ts
@@ -1,46 +1,6 @@
 import axios from "axios";
 
 export async function getPokemon(pokemonId: number) {
-//     const getPokemonQuery = `query pokemon{
-//   pokemon(name: "${pokemonName}") {
-//     id
-//     name
-//     species{
-//       url
-//       name
-//     }
-//     location_area_encounters
-//     abilities{
-//       ability{
-//         url
-//         name
-//       }
-//     }
-//     base_experience
-//     forms{
-//       url
-//       name
-//     }
-//     stats{
-//       base_stat
-//       stat{
-//         name
-//       }
-//     }
-//     types{
-//       slot
-//       type{
-//         name
-//       }
-//     }
-//     sprites{
-//       front_default
-//       back_default
-//     }
-//     weight
-//   }
-// }
-// `
     return axios({
         url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}`,
         method: 'get',
