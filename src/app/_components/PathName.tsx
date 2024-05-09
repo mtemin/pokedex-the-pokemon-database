eed9c889b5155c51eafe8508cf071d297546975d
Mehmet Emin <mteminn@gmail.com>
2024-05-10 02:27:08 +0300

    new client component for url pathname

---
+++ src/app/_components/PathName.tsx
@@ -0,0 +1,33 @@
+"use client"
+import {usePathname} from "next/navigation";
+
+function PathName({isShort}: { isShort: boolean }) {
+    let path: string = usePathname()
+    if (isShort) {
+        path = `${path.split('/')[2]}`
+        // console.log(path.split('/')[2])
+    }
+
+    let nonLatinDetected: boolean = false;
+
+    if (path.includes('%')) {
+        nonLatinDetected = true;
+    }
+
+    return (
+        <>
+            <span className="underline"> {path}</span>
+            {nonLatinDetected
+                ? <p className="bg-yellow-400 text-[var(--background)] rounded px-4">
+                    Non-latin characters detected in the text you
+                    entered, try again with
+                    only latin
+                    characters</p>
+                : <></>
+
+            }
+        </>
+    );
+}
+
+export default PathName;
