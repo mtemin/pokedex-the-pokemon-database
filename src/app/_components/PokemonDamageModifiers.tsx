5048d3105e3409cae9dce16f0c940bffca01fa5b
Mehmet Emin <mteminn@gmail.com>
2024-05-14 00:39:11 +0300

    fix for responsive width

--- src/app/_components/PokemonDamageModifiers.tsx
+++ src/app/_components/PokemonDamageModifiers.tsx
@@ -23,8 +23,8 @@ export default function PokemonDamageModifiers({pokemonId}: { pokemonId: number 
 
     return (
         <aside id="sidebar"
-               className="m-3 max-[1000px]:mt-0 overflow-y-scroll col-span-2 w-auto max-[1000px]:w-[calc(50%-1.25rem)] max-[1000px]:mr-1.5 h-[calc(100vh-90px)] flex flex-col justify-between scrollbar-thumb-sky-700 scrollbar-track-sky-300 max-[1000px]:order-2">
-            <div className="bg-[var(--background-card)] rounded-t">
+               className="m-3 rounded max-[1000px]:mt-0 overflow-y-scroll col-span-2 w-auto max-[1000px]:w-[calc(50%-1.25rem)] max-[1000px]:mr-1.5 h-[calc(100vh-100px)] flex flex-col justify-between  max-[1000px]:order-2">
+            <div className="bg-[var(--background-card)] rounded">
                 {/*<Image*/}
                 {/*    src="/Pokédex_logo.png"*/}
                 {/*    width={200}*/}
@@ -34,7 +34,7 @@ export default function PokemonDamageModifiers({pokemonId}: { pokemonId: number 
                 {/*    priority*/}
                 {/*/>*/}
             </div>
-            <ul className="bg-[var(--background-card)] flex flex-col h-full w-full rounded-b py-4 px-2">
+            <ul className="bg-[var(--background-card)] flex flex-col h-full w-full rounded py-4 px-2">
                 <div className="flex justify-center">
                     {typeData
                         ?
