6860305aecc34129adf41386c7025bb4cfbbae7d
Mehmet Emin <mteminn@gmail.com>
2024-05-14 00:38:51 +0300

    removed skeletons

--- src/app/_components/PokedexByTypeList.tsx
+++ src/app/_components/PokedexByTypeList.tsx
@@ -20,18 +20,12 @@ function PokedexByTypeList() {
         return result[result.length - 2];
     }
 
+    console.log(pokedexByTypeData)
+
     return (
         <section id="pokedex-by-type">
-            {isPokedexByTypeLoading &&
-                <>
-                    {Array.from({length: 25}, (_, i) => (
-                        <Skeleton key={i} className="h-10 w-full my-2 rounded-none"/>
-                    ))
-                    }
-                </>
-            }
             {pokedexByTypeData &&
-                pokedexByTypeData.pokemon.map((pokemon: any) =>
+                pokedexByTypeData.pokemon.map((pokemon: { pokemon: { name: string, url: string }, slot: number }) =>
                     <Link key={pokemon.pokemon.name} href={`/pokemon/${getPokemonIdFromURL(pokemon.pokemon.url)}`}>
                         <article style={{backgroundColor: `var(--type-${selectedType})`}}
                                  className="flex flex-col justify-center items-center my-2 p-2 bg-[var(--pokemon-bg)] hover:shadow-inside transition-all duration-300">
