7b0f60ee7e6bbd8e38e05faad0a37710deea7a48
Mehmet Emin <mteminn@gmail.com>
2024-05-14 16:22:37 +0300

    fixed error caused by client and server random pokemon url mismatch

--- src/app/_components/PokemonCard.tsx
+++ src/app/_components/PokemonCard.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
+import {currentPokemonSlice, setCurrentPokemon} from "@/lib/features/currentPokemonSlice";
+import {useAppDispatch, useAppSelector} from "@/lib/hooks";
+
 
 function PokemonCard({pokemon}: { pokemon: any }) {
+
+    function getRandomInt(min: number, max: number) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+    }
+
+    const dispatch = useAppDispatch();
+    let currentPokemon = useAppSelector((state: any) => state.currentPokemon.value)
     return (
-        <Link key={pokemon.id} href={`/pokemon/${pokemon.id}`}>
+        // <Link key={pokemon.id} href={`/pokemon/${pokemon.id}`}
+        <Link key={pokemon.id} href={{pathname: `/pokemon/${pokemon.id}`, query: {id: pokemon.id}}}
+              onClick={() => dispatch(setCurrentPokemon(pokemon.id))}>
             <article
                 className="flex flex-col justify-center items-center border-2 border-[var(--pokemon-bg-shadow)] rounded-2xl m-4 p-4 bg-[var(--pokemon-bg)] hover:shadow-inside transition-all duration-300">
                 <Image src={pokemon.image} alt="Pokemon front stripe" width={128} height={128}
