212dd1c18205bac2e015f7f8eaaa74a507803949
Mehmet Emin <mteminn@gmail.com>
2024-05-14 16:20:50 +0300

    remove unneccesary code and fix pokemon page height

--- src/app/(pages)/pokemon/[id]/page.tsx
+++ src/app/(pages)/pokemon/[id]/page.tsx
@@ -6,17 +6,11 @@ import PokemonStats from "@/app/_components/PokemonStats";
 import Navbar from "@/app/_components/Navbar";
 import usePokemonQuery from "@/app/_hooks/usePokemonQuery";
 
-function Pokemon({params}: any) {
-    let pokemonId = params.id;
-    const {isError: isPokemonError} = usePokemonQuery(pokemonId);
-    let mehmet = true;
+function Pokemon({params}: { params: { id: number } }) {
 
     return (
-        <section className="h-screen overflow-hidden max-[1000px]:overflow-y-scroll">
-            {/*{mehmet &&*/}
-            {/*    <div*/}
-            {/*        className="absolute bg-rose-500 w-[50%] h-[25%] z-[3] translate-x-[-50%] translate-y-[-50%] left-[50%] top-[50%] rounded-2xl flex justify-center items-center">eq</div>*/}
-            {/*}*/}
+        <section className="h-[98vh] overflow-hidden max-[1000px]:overflow-y-scroll">
+
             <div className="mx-3">
                 <Navbar/>
             </div>
