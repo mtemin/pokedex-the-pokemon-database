3cd8c00e28925c0308c803bc388d37abe718563d
Mehmet Emin <mteminn@gmail.com>
2024-05-14 16:22:11 +0300

    fixed error caused by client and server random pokemon url mismatch

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from "next/link";
 import GoToPokemon from "@/app/_components/GoToPokemon";
 import IconArrowUpDown from "@/app/_components/Icon-Arrow-Up-Down";
+import {useAppDispatch, useAppSelector} from "@/lib/hooks";
+import {setCurrentPokemon} from "@/lib/features/currentPokemonSlice";
 
 function Navbar() {
-    function getRandomInt(min: number, max: number) {
+
+    const dispatch = useAppDispatch();
+    let currentPokemon = useAppSelector((state: any) => state.currentPokemon.value)
+
+    const getRandomInt = (min: number, max: number) => {
         const minCeiled = Math.ceil(min);
         const maxFloored = Math.floor(max);
         return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
@@ -12,29 +18,36 @@ function Navbar() {
 
     function toggleNavbar() {
         const navbar: Element | null = document.querySelector('#navbar');
-        if (navbar)
+        if (navbar) {
             navbar.classList.toggle('max-sm:mt-[-12rem]')
+            navbar.classList.toggle('max-sm:mt-[0]')
+        }
     }
 
-    let randomPokemonURL = `/pokemon/${getRandomInt(0, 1025)}`
+    const [randomPokemonId, setRandomPokemonId] = useState<null | number>(null)
+    useEffect(() => {
+        dispatch(setCurrentPokemon(getRandomInt(0, 1025)));
+        setRandomPokemonId(getRandomInt(0, 1025))
+    }, [])
+
     return (
-        <nav id="navbar" style={{marginTop: `${toggleNavbar}`}}
-             className="rounded max-sm:fixed max-sm:mx-[-2rem] max-sm:mt-0 max-sm:rounded-none bg-[var(--background-card)] z-10 mt-3 w-full max-sm:border-b border-[var(--foreground-card)] transition-all duration-500 ">
-            <ul className="flex max-sm:flex-col justify-center items-center">
+        <nav id="navbar"
+             className="rounded max-sm:rounded-b flex justify-center mx-auto bg-[var(--background-card)] z-10 mt-3 w-full max-sm:mt-[0] transition-all duration-500 ">
+            <ul className="flex justify-center items-center max-sm:flex-col w-full my-1">
                 <Link href="/"
                       className="px-4 py-2 text-2xl  max-[760px]:text-lg max-[760px]:px-2 max-[760px]:py-1 text-center font-bold text-[var(--foreground-card)] hover:text-[var(--pokemon-fg)] cursor-pointer capitalize">Home</Link>
                 <Link href="/pokedex/"
                       className="px-4 py-2 text-2xl  max-[760px]:text-lg max-[760px]:px-2 max-[760px]:py-1 text-center font-bold text-[var(--foreground-card)] hover:text-[var(--pokemon-fg)] cursor-pointer capitalize">Pokedex</Link>
-                <Link href={randomPokemonURL}
+                <Link href={`/pokemon/${randomPokemonId}`}
                       className="px-4 py-2 text-2xl  max-[760px]:text-lg max-[760px]:px-2 max-[760px]:py-1 text-center font-bold text-[var(--foreground-card)] hover:text-[var(--pokemon-fg)] cursor-pointer capitalize">Random
                     Pokemon</Link>
                 <Link href="/types"
                       className="px-4 py-2 text-2xl  max-[760px]:text-lg max-[760px]:px-2 max-[760px]:py-1 text-center font-bold text-[var(--foreground-card)] hover:text-[var(--pokemon-fg)] cursor-pointer capitalize">Pokemons
                     By Type</Link>
                 <GoToPokemon/>
-                <span onClick={toggleNavbar} className=" ml-auto my-1 mr-2">
+                <span onClick={toggleNavbar} className="hidden max-sm:block ml-auto my-1 mr-2">
                     <IconArrowUpDown
-                        className="w-6 h-6 p-1 rounded-full border border-[var(--foreground-card)] text-[var(--foreground-card)] hidden max-sm:block cursor-pointer"/>
+                        className="w-6 h-6 p-1 rounded-full border border-[var(--foreground-card)] text-[var(--foreground-card)] cursor-pointer"/>
                 </span>
             </ul>
         </nav>
